95049fd85e038ebcf918b4d6fe7eb3d19e268107
AlninoDP <alninodp@gmail.com>
2024-07-11 22:36:22 +0700

    added method to fetch user contact

--- lib/services/user_data_service.dart
+++ lib/services/user_data_service.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:smart_jakarta/exception/exception.dart';
 import 'package:smart_jakarta/models/user_bio.dart';
+import 'package:smart_jakarta/models/user_contact.dart';
 import 'package:smart_jakarta/network/api.dart';
 
 class UserDataService {
@@ -18,6 +19,30 @@ class UserDataService {
         final userData = resBody['data'] as Map<String, dynamic>;
 
         return UserBio.fromJson(userData);
+      }
+    } on TimeoutException catch (_) {
+      throw ReqTimeoutException(
+          'Error Connecting to Server, Request Timed Out');
+    } on ClientException catch (_) {
+      throw AuthException('Error Connecting to Server');
+    } catch (e) {
+      throw AuthException(e.toString());
+    }
+    return null;
+  }
+
+  Future<List<UserContact>?> fetchUserContact() async {
+    try {
+      final response = await _network.getData('/user-contacts');
+
+      if (response.statusCode == 200) {
+        final resBody = jsonDecode(response.body);
+        final data = resBody['data'] as List;
+
+        final contactList =
+            data.map((json) => UserContact.fromJson(json)).toList();
+
+        return contactList;
       }
     } on TimeoutException catch (_) {
       throw ReqTimeoutException(
